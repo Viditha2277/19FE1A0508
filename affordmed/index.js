414db7139ffdf5f7ae302bd98a6467477fe7a5c2
Viditha2277 <vidithaavuthu@gmail.com>
2022-12-30 21:27:50 +0530

    third

--- affordmed/index.js
+++ affordmed/index.js
@@ -9,6 +9,7 @@ app.use(bodyParser.json());
 
 const lis = ["/primes","/fibo","/odd","/rand"];
 var r = [];
+var s = [];
 
 app.get("/numbers", async (req,res) => {
   var p = req.query.url;
@@ -19,9 +20,25 @@ app.get("/numbers", async (req,res) => {
     var x = url.parse(p[i],true);
     console.log(x.pathname);
     
-    r.push(x);
+    r.push(x.pathname);
+    if(x.pathname=="/primes"){
+      var e = [2, 3, 5, 7, 11, 13];
+      s = s.concat(e);
+    }
+    if(x.pathame=="/fibo"){
+      var e = [1, 1, 2, 3, 5, 8, 13, 21];
+      s = s.concat(e);
+    }
+    if(x.pathname=="/odd"){
+      var e = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23];
+      s = s.concat(e);
+    }
+    if(x.pathname=="/rand"){
+      var e = [5, 17, 3, 19, 76, 24, 1, 5, 10, 34, 8, 27, 7];
+      s = s.concat(e);
+    }
   }
-  return res.json(r);
+  return res.json({numbers: s});
   
 });
 
